a6cf4cad204e96a44f63af51765a5d0bd33640e5
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-07-11 11:29:37 +0700

    detected hand

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.79 // confidence threshold for predictions.
+};
+
 // Get user media base on multiple browser
 navigator.getUserMedia =
   navigator.getUserMedia ||
@@ -20,12 +28,20 @@ handTrack.startVideo(video).then(status => {
       { video: {} },
       stream => {
         video.srcObject = stream;
+        setInterval(runDetection, 1000);
       },
       err => console.log(err)
     );
   }
 });
 
-handTrack.load().then(lmodel => {
+function runDetection() {
+  model.detect(video).then(predictions => {
+    console.log(predictions);
+  });
+}
+
+handTrack.load(modelParams).then(lmodel => {
+  // Returns a model object
   model = lmodel;
 });
